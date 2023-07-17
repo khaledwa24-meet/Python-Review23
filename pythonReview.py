ac8f55e4c3f32dcbb6df8142fdd00efbb8b8b883
khaledwa24-meet <139680869+khaledwa24-meet@users.noreply.github.com>
2023-07-17 09:56:34 +0300

    Update pythonReview.py

--- pythonReview.py
+++ pythonReview.py
@@ -1,10 +1,10 @@
 def create_youtube_video(title, description):
 	video = {"title" : title, "description" : description, "likes" : 0 , "comments" : {}, "dislikes" : 0}
-	return dic
+	return video
 
 def like(video):
 	if ("likes" in video):
-		vidoe["likes"] += 1
+		video["likes"] += 1
 		return video
 
 def dislike():
@@ -15,9 +15,10 @@ def add_comment(video, username, comment_text):
 	video["comments"][username] = comment_text
 	return video
 
-userTitle = input(" enter tittle")
-userDescription = input("enter description")
+userTitle = input(" enter tittle: ")
+userDescription = input("enter description: ")
 video = create_youtube_video(userTitle , userDescription)
 
 for i in range(496):
-	print(likes(video))
+	like(video)
+print(video)
